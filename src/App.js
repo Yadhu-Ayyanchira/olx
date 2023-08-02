ea1518235af4790f793dc42b82f31cda5bdc0a27
Yadhu <yadhukrishnanas@gmail.com>
2023-08-02 11:33:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './Pages/Signup'
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
